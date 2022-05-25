7baaceaaf48dd2e4f21404a168465fdb20c14333
Lydia Eluro <elurolydia@gmail.com>
2022-05-25 02:07:08 +0100

    added new feature to congratulatory message

--- index.js
+++ index.js
@@ -5,129 +5,16 @@ let button = document.getElementById('#submitButton');
 button.addEventListener("click", validateForm);
 
 
-
-
-
-
-
-
-
-
-// function validateForm(e){
-//     e.preventDefault();
-
-//     let divBox1 = document.getElementById('firstBox');
-//     let inputValue1 =document.myForm.FirstName.value;
-//     let inputBox1 = document.getElementById('first_Name');
-//     let errorIcon1 = document.getElementById('firstErrorIcon');
-//     let errorMessage1 = document.getElementById('errorMessage1');
-//     if (inputValue1 == ''){ 
-//         divBox1.style.border = 'hsl(0, 100%, 74%)  2px solid';
-//         errorIcon1.style.visibility = 'visible';
-//         errorMessage1.style.visibility = 'visible';
-//         inputBox1.placeholder = '';
-//     }else if(inputValue1 != ''){
-//         divBox1.style.border = 'hsl(246, 25%, 77%) 2px solid';
-//         errorIcon1.style.visibility = 'hidden';
-//         errorMessage1.style.visibility = 'hidden';
-//         inputBox1.placeholder = '';
-//     }
-
-
-//     let divBox2 = document.getElementById('secondBox');
-//     let inputValue2 =document.myForm.LastName.value;
-//     let inputBox2 = document.getElementById('second_Name');
-//     let errorIcon2 = document.getElementById('secondErrorIcon');
-//     let errorMessage2 = document.getElementById('errorMessage2');
-//     if (inputValue2 == ''){ 
-//         divBox2.style.border = 'hsl(0, 100%, 74%)  2px solid';
-//         errorIcon2.style.visibility = 'visible';
-//         errorMessage2.style.visibility = 'visible';
-//         inputBox2.placeholder = '';
-//     }else if(inputValue2 != ''){
-//         divBox2.style.border = 'hsl(246, 25%, 77%) 2px solid';
-//         errorIcon2.style.visibility = 'hidden';
-//         errorMessage2.style.visibility = 'hidden';
-//         inputBox2.placeholder = '';
-//     }
-
-
-//     let divBox3 = document.getElementById('thirdBox');
-//     let inputValue3 =document.myForm.Email.value;
-//     let inputBox3 = document.getElementById('email_Address');
-//     let errorIcon3 = document.getElementById('thirdErrorIcon');
-//     let errorMessage3 = document.getElementById('errorMessage3');
-//     // let patn = [a-z0-9.%+-]+@[a-z0-9.-]+\.[a-z]{2,}$];
-//     let patn =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-//     if (!patn.test(inputValue3)){ 
-//         divBox3.style.border = 'hsl(0, 100%, 74%)  2px solid';
-//         errorIcon3.style.visibility = 'visible';
-//         errorMessage3.textContent = 'Looks like this is not an email';
-//         errorMessage3.style.visibility = 'visible';
-//         errorMessage3.style.fontSize = '0.7rem';
-//         // document.querySelectorAll('input')[2].style.color = "hsl(0, 100%, 74%)";
-//         document.querySelectorAll('input')[2].style.color = "black";
-//         inputBox3.placeholder = '';
-//         if(inputValue3 == ''){
-//             errorMessage3.textContent = 'Email cannot be empty';
-//         }
+// function validateEmail(email){
+//     const patn =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     if (!patn.test(email)){ 
+//         console.log("invalid email");
         
-//         }else{
-//             // }else if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputValue3)){
-//             divBox3.style.border = 'hsl(246, 25%, 77%) 2px solid';
-//             errorIcon3.style.visibility = 'hidden';
-//             errorMessage3.style.visibility = 'hidden';
-//             inputBox3.placeholder = '';
-//             // document.querySelectorAll('input')[2].style.color = "black";
-//             // document.querySelectorAll('input').style.backgroundColor = "black";
-//     }
-
-
-//     let divBox4 = document.getElementById('forthBox');
-//     let inputValue4 =document.myForm.Password.value;
-//     let inputBox4 = document.getElementById('password');
-//     let errorIcon4 = document.getElementById('forthErrorIcon');
-//     let errorMessage4 = document.getElementById('errorMessage4');
-//     if (inputValue4 == ''){ 
-//         divBox4.style.border = 'hsl(0, 100%, 74%)  2px solid';
-//         errorIcon4.style.visibility = 'visible';
-//         errorMessage4.style.visibility = 'visible';
-//         inputBox4.placeholder = '';
-//     }else if(inputValue4 != ''){
-//         divBox4.style.border = 'hsl(246, 25%, 77%) 2px solid';
-//         errorIcon4.style.visibility = 'hidden';
-//         errorMessage4.style.visibility = 'hidden';
-//         inputBox4.placeholder = '';
-//     }
-
-//     if (true){
-//         showCongratulatoryMessage();
 //     }else{
-//         return;
+//     // }else if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputValue3)){
+//         console.log("Valid email found!!!");
 //     }
-    
-    
 // }
-
-
-
-
-function validateEmail(email){
-    const patn =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!patn.test(email)){ 
-        console.log("invalid email");
-        
-    }else{
-    // }else if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputValue3)){
-        console.log("Valid email found!!!");
-    }
-}
-
-
-function showCongratulatoryMessage () {
-popUp.style.display = 'flex';
-}
 
 
 let divBox = document.querySelectorAll('.div4icons');
@@ -135,7 +22,16 @@ let inputBox =document.querySelectorAll('input');
 let errorIcon = document.querySelectorAll('i');
 let errorMessage = document.querySelectorAll('.errorMsg');
 
+let congratulatoryInfo = document.querySelectorAll('.congratulatoryInputValue');
+let popButton = document.getElementById('popUpButton');
+let myForm = document.getElementById('myForm');
 
+popButton.addEventListener('click', popUpOut);
+function popUpOut(){
+    popUp.style.display = 'none';
+    myForm.reset();
+    
+}
 
 function validateForm(e){
     e.preventDefault();
@@ -145,13 +41,13 @@ function validateForm(e){
             divBox[i].style.border = 'hsl(0, 100%, 74%)  2px solid';
             errorIcon[i].style.visibility = 'visible';
             errorMessage[i].style.visibility = 'visible';
-            inputBox[i].placeholder = '';
+            // inputBox[i].placeholder = '';    
 
         }else if(inputBox[i].value !== ''){
             divBox[i].style.border = 'hsl(246, 25%, 77%) 2px solid';
             errorIcon[i].style.visibility = 'hidden';
             errorMessage[i].style.visibility = 'hidden';
-            inputBox[i].placeholder = '';
+            // inputBox[i].placeholder = '';
             if (i === 2) {
                 let patn =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
                 if (!patn.test(inputBox[i].value)) {
@@ -160,18 +56,24 @@ function validateForm(e){
                 errorMessage[i].textContent = 'Looks like this is not an email';
                 errorMessage[i].style.visibility = 'visible';
                 errorMessage[i].style.fontSize = '0.7rem';
-                inputBox[2].style.color = "black";
-                inputBox[i].placeholder = '';
-                
+                inputBox[i].style.color = "black";
+                // inputBox[i].placeholder = '';
+                }else if(inputBox[i].value === ''){
+                    errorMessage[i].style.visibility = 'visible';
                 }
             }
         }
+        congratulatoryInfo[i].textContent = inputBox[i].value;
+
         if (errorMessage[i].style.visibility === 'hidden' && errorIcon[2].style.visibility === 'hidden'){
             showCongratulatoryMessage();
-        }
-        
-    }
-    
-       
+        }    
+    }    
 }
+
+
+function showCongratulatoryMessage () {
+    popUp.style.display = 'flex';
+}
+
 
